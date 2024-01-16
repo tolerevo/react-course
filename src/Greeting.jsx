66fd28d55614d8194ee2ee4e184524cb92c02f82
tolerevo <tolerevo@hotmail.com>
2024-01-16 05:41:41 -0600

    default props

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 
 export function Greeting() {
-  return (
-    <h1>Componente de React</h1>
-  );
+  return <h1>Componente de React</h1>;
 }
 
-export function UserCard() {
-  return <h1>User Card</h1>
+export function UserCard({ name, amount, married, points }) {
+  return<>
+  <h1>User Card</h1>
+  <h2 style={{textTransform: "uppercase", fontStyle: "italic"}}>{name}</h2>
+  <ul style={{listStyle: "inside"}}>
+    <li>{amount}$</li>
+    <li>{points}</li>
+
+  </ul>
+  </> 
 }
--- src/index.js
+++ src/index.js
@@ -2,18 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Greeting, UserCard } from "./Greeting";
 import Product, { NavBar } from "./Product";
+import Button from "./Button";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
-    <Greeting />
-    <Greeting />
-    <UserCard />
-    <Product />
-    <Product />
-    <NavBar />
-    <Greeting />
-    <UserCard />
-  </>
+    {/* <UserCard
+      name="Ryan Ray"
+      amount={3000}
+      married={true}
+      points={[99, 33, 3, 2, 2]}
+      address={{ street: "123 Main Street", city: "NewYork" }}
+      greet={function () {
+        alert("Hello");
+      }}
+    />
+
+    <UserCard
+      name="Joe Mcmillan"
+      amount={2300}
+      married={false}
+      points={[99, 33, 3, 2, 2]}
+      address={{ street: "123 Main Street", city: "NewJersey" }}
+    /> */}
+    <Button />
+  </>,
 );
