84fc75c8b0e56cb73efabc2bcdf8fa905e18439e
tolerevo <tolerevo@hotmail.com>
2024-01-17 12:44:05 -0600

    use efect

--- src/index.js
+++ src/index.js
@@ -1,38 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
-// eslint-disable-next-line
 import { Post } from "./Post";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const users = [
-  {
-    id: 1,
-    name: "Ryan ray",
-    image: "https://robohash.org/user1",
-  },
-  {
-    id: 2,
-    name: "Ryan Roberts",
-    image: "https://robohash.org/user2",
-  },
-  {
-    id: 3,
-    name: "Ryan Gosling",
-    image: "https://robohash.org/user3",
-  },
-  ,
-];
-
 function Counter() {
   const [mensaje, setMensaje] = useState("");
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log("render");
+  }, []);
 
   return (
     <div>
       <input onChange={(e) => setMensaje(e.target.value)} />
-      <button onClick={() => {
-        alert('El mensaje es:' + mensaje)
-      }}>Save</button>
+      <button
+        onClick={() => {
+          alert("El mensaje es:" + mensaje);
+        }}
+      >
+        Save
+      </button>
+
+      <hr />
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>Incrementar</button>
     </div>
   );
 }
